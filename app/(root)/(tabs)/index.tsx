23bf24a16a819a690b904453dd9d7bf487c32426
Spyder1022 <dhuwarakesh0101@gmail.com>
2025-02-19 10:06:00 +0530

    Initial commit: Add .gitignore, README.md, app.json, and other project files

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Link href="/sign-in" > Sign up</Link>
+        <Link href="/explore" > Explore</Link>
+        <Link href="/properties/1" > Properties</Link>
+        <Link href="/profile" > Profile</Link>
+
+    </View>
+  );
+}
