f49ff65bf0a84bcf7c000517f9bd73ea20c379b7
Dhuwarakesh <dhuwatck@gmail.com>
2025-03-02 03:04:10 +0530

    Sign Page Complete

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,21 +1,20 @@
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
-
+import { Link } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Link href="/sign-in" > Sign up</Link>
-        <Link href="/explore" > Explore</Link>
-        <Link href="/properties/1" > Properties</Link>
-        <Link href="/profile" > Profile</Link>
-
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text className={"font-bold my-10 font-rubik text-3xl"}>Welcome to ReState</Text>
+            <Link href="/sign-in">Sign up</Link>
+            <Link href="/explore">Explore</Link>
+            <Link href="/properties/1">Properties</Link>
+            <Link href="/profile">Profile</Link>
+        </View>
+    );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,36 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
+import icons from '@/constants/icons';
 
 const SignIn = () => {
+    const handLogin = () => {
+    };
+
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
-    )
-}
-export default SignIn
+        <SafeAreaView className="bg-white h-full">
+            <ScrollView contentContainerClassName="h-full">
+                <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain" />
+                <View>
+                    <Text className="text-base text-center uppercase font-rubik text-black-200"> Welcome to ReState</Text>
+                    <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2"> Let's Get You Closer to {"\n"}
+                        <Text className="text-primary-300"> Your Ideal Home </Text>
+                    </Text>
+                    <Text className="text-lg font-rubik text-black-200 text-center mt-12"> Login to ReState with Google </Text>
+
+                    <TouchableOpacity onPress={handLogin} className="bg-white shadow-md shadow-zinc-200 rounded-full w-11/12 py-4 mt-5 self-center">                        <View className="flex flex-row  items-center justify-center">
+                            <Image
+                                source={icons.google}
+                                className="w-5 h-5"
+                                resizeMode="contain" />
+                            <Text className="text-lg font-rubik-medium text-black-300 ml-2">Continue with Google</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default SignIn;
